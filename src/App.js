6d5647e0d39cdd3102bdae1452e83f58a142f48a
unknown <kgeluza@mail.ru>
2022-11-06 19:51:49 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import Card from "./components/Card"
 import './App.css';
 
-const card1 = {
+const cards = [{
   image: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
   alt: "картинка 200",
   title: "Card title",
   text: "Some quick example text to build on the card title and make up the bulk of the card's content"
-}
-
-const card2 = {
+},
+{
   image: "",
   alt: "",
   title: "Special title treatment",
   text: "With supporting text below as a natural lead-in to additional content"
-}
+}]
 
 function App() {
   return (
     <main className="App">
-    <Card props={card1}/>
-    <Card props={card2}/>
+    {cards.map(card => {
+      return(
+    <Card props={card}/>
+      )
+    }
+  )}
     </main>
   );
 }
