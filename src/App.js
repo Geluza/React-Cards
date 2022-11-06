ca6facf6f693c012b0d4ab26175e7a92f8099ee9
unknown <kgeluza@mail.ru>
2022-11-06 12:54:48 +0400

    commit 06/11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import Card from "./components/Card"
 import './App.css';
+
+const card1 = {
+  image: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+  alt: "картинка 200",
+  title: "Card title",
+  text: "Some quick example text to build on the card title and make up the bulk of the card's content"
+}
+
+const card2 = {
+  image: "",
+  alt: "",
+  title: "Special title treatment",
+  text: "With supporting text below as a natural lead-in to additional content"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+    <Card props={card1}/>
+    <Card props={card2}/>
+    </main>
   );
 }
 
